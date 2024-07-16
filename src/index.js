57b75e9201aabf5a876d0cb55db5e9cbd9b28b07
Rodgers Chesoni <rodgerschesoni@gmail.com>
2024-07-16 08:25:01 +0300

    add toy tale

--- src/index.js
+++ src/index.js
@@ -1,15 +1,76 @@
-let addToy = false;
+document.addEventListener('DOMContentLoaded', () => {
+  const toyCollection = document.getElementById('toy-collection');
+  const addToyForm = document.getElementById('add-toy-form');
+  const toysUrl = 'http://localhost:3000/toys';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
+  // Fetch and display toys
+  fetch(toysUrl)
+    .then(response => response.json())
+    .then(toys => {
+      toys.forEach(toy => {
+        renderToy(toy);
+      });
+    });
+
+  // Add toy info to the card
+  function renderToy(toy) {
+    const toyCard = document.createElement('div');
+    toyCard.className = 'card';
+    toyCard.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar" />
+      <p>${toy.likes} Likes</p>
+      <button class="like-btn" id="${toy.id}">Like </button>
+    `;
+    toyCollection.appendChild(toyCard);
+
+    // Add event listener to the like button
+    const likeBtn = toyCard.querySelector('.like-btn');
+    likeBtn.addEventListener('click', () => {
+      increaseLikes(toy);
+    });
+  }
+
+  // Add a new toy
+  addToyForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const toyName = event.target.name.value;
+    const toyImage = event.target.image.value;
+    const newToy = { name: toyName, image: toyImage, likes: 0 };
+
+    fetch(toysUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    })
+    .then(response => response.json())
+    .then(toy => {
+      renderToy(toy);
+      addToyForm.reset();
+    });
   });
+
+  // Increase a toy's likes
+  function increaseLikes(toy) {
+    toy.likes += 1;
+
+    fetch(`${toysUrl}/${toy.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ likes: toy.likes })
+    })
+    .then(response => response.json())
+    .then(updatedToy => {
+      const toyCard = document.getElementById(updatedToy.id).parentNode;
+      const likesP = toyCard.querySelector('p');
+      likesP.textContent = `${updatedToy.likes} Likes`;
+    });
+  }
 });
+
